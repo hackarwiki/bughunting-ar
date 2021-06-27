3b54ee0e09e42b055c8c565902292a1a4daba885
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2021-06-27 04:21:59 +0200

    Update conv.py

--- scripts/conv.py
+++ scripts/conv.py
@@ -1,17 +1,59 @@
-#!/usr/bin/env python3
+import re,sys
 
-import sys
-
-
-
-    #print(i)
-for test in sys.stdin:
-    test = test.rstrip()
-    if test.startswith('#'):
-        txt = test.count('#')
-        if txt:
-            t = txt * '#'
-            m = test.split(' ')[0].replace(t,f'<h{txt} align="right">')
-            c = test.split(' ')
-            c[0] = m
-            print(''.join(c))
+# made with ❤️ by @knassar702
+# usage: cat file.md | python3 conv.py
+class arMarkdown:
+    def __init__(self,data):
+        self.data = data
+    def header(self):
+        c = self.data.split(' ')[0].count('#')
+        if c == 0 or c > 6:
+            return self.data
+        else:
+            v = f'<h{c} dir="rtl" align="right">{"".join(self.data.split(" ")[1:])}</h{c}>'
+            self.data = v
+            return v
+    def comment(self):
+        c = self.data.split(' ')[0].count('>')
+        if c > 0 :
+            f = f"<blackqoute dir='rtl' align='right'>{''.join(self.data.split(' ')[1:])}</blackqoute>"
+            self.data = f
+            return f
+        return self.data
+    def image(self):
+        txt = self.data
+        sub = ''
+        r = re.search(r'!\[[^\]]*\](.*?)\s*("(?:.*[^"])")?\s*\)',txt)
+        if r:
+            c = r.group()
+            alt = re.findall(r'!\[(.*?)\](|. .)[(].*[)]',c)
+            link = re.findall(r'[(].*[)]',c)[0]
+            link = link.replace(link[0],'').replace(link[-1],'')
+            sub += f"<img src='{link}' alt='{alt[0][0]}' style='float: right;' />"
+            self.data = sub
+        return self.data
+    def links(self):
+        txt = self.data
+        sub = ''
+        r = re.search(r'\[[^\]]*\](.*?)\s*("(?:.*[^"])")?\s*\)',txt)
+        if r:
+            c = r.group()
+            # get the content of []
+            name = re.findall(r'!\[(.*?)\](|. .)[(].*[)]',c)
+            # get the content of ()
+            link = re.findall(r'[(].*[)]',c)[0]
+            link = link.replace(link[0],'').replace(link[-1],'')
+            # add the link and name
+            sub += f'<a style="float:right" href="{link}">{name}</a>'
+            self.data = sub
+        return txt
+    def start(self):
+        self.image()
+        self.header()
+        self.comment()
+        self.links()
+        print(self.data)
+for data in sys.stdin:
+    data = data.rstrip()
+    c = arMarkdown(data)
+    c.start()
